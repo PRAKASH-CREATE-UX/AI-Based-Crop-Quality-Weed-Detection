3decc442946f205a5e21998e0eedbd0462171615
PRAKASH-CREATE-UX <tammireddyprakash7@gmail.com>
2026-07-08 09:16:47 +0530

    Add files via upload

---
+++ agri_ai_project_5/userauth/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def user_profile(request):
+    return render(request, 'profile.html', {'user': request.user})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
